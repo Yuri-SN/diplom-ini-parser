f88114703c2d56ef19201e9102da8022d00720de
Yuri Solyanik <jura.s.n@gmail.com>
2025-01-24 11:39:00 +0500

    Добавил основную структуру, в которой будут хранится данные

--- ini_parser.h
+++ ini_parser.h
@@ -1,8 +1,12 @@
 #pragma once
 
+#include <map>
 #include <string>
 
 class ini_parser {
 public:
   ini_parser(const std::string& filename);
+
+private:
+  std::map<std::string, std::map<std::string, std::string>> m_data;
 };
--- main.cpp
+++ main.cpp
@@ -1,3 +1,13 @@
+#include <iostream>
+#include <ostream>
+
+#include "ini_parser.h"
+
 int main() {
+  try {
+    ini_parser parser("example.ini");
+  } catch (const std::exception& e) {
+    std::cerr << "Error: " << e.what() << std::endl;
+  }
   return 0;
 }
