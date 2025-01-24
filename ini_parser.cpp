dedf932f25dd212c2fcb74ba059f7b37fc94a83d
Yuri Solyanik <jura.s.n@gmail.com>
2025-01-24 11:21:05 +0500

    Добавил пустой класс парсера

---
+++ ini_parser.cpp
@@ -0,0 +1,3 @@
+#include "ini_parser.h"
+
+ini_parser::ini_parser(const std::string& filename) {}
---
+++ ini_parser.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <string>
+
+class ini_parser {
+public:
+  ini_parser(const std::string& filename);
+};
